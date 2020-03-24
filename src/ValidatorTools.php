82c504e44ec3028d9b6959f783889da95a12a694
超超 <cc@chaochaos-iMac.local>
2020-03-24 14:27:47 +0800

    cc

--- src/ValidatorTools.php
+++ src/ValidatorTools.php
@@ -56,7 +56,13 @@ class ValidatorTools
 
         foreach ($this->rules as $key => $rule) {
             if(!in_array($key, $rules)) {
-                unset($this->rules[$key]);
+                if (is_array($rules[$key])){
+                    $this->rules[$key] = $rules[$key][0];
+                }else if (is_array(explode('|',$rules[$key]))){
+                    $this->rules[$key] = $rules[$key];
+                }else{
+                    unset($this->rules[$key]);
+                }
             }
         }
         //$this->rules    = array_only($this->rules, $rules);
